0ee98870b34350d864ce5de2d4fa5a2ebd694d02
SantiagoPapa14 <papa.santiago321@gmail.com>
2024-10-15 15:16:05 -0300

    Tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <body>
+    <div>
+      <NuxtPage />
+    </div>
+  </body>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+  </div>
+</template>
