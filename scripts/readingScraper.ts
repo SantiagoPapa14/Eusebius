03ef30e23979458593b4a40bce322138516431a8
SantiagoPapa14 <papa.santiago321@gmail.com>
2024-10-17 17:10:55 -0300

    Working reading scraper

--- scripts/readingScraper.ts
+++ scripts/readingScraper.ts
@@ -1,19 +1,43 @@
 function getDateInScraperFormat(): string {
   // Format (MMDDYYYY) For example (04142003 is my birthday)
   const date = new Date();
-  const year = date.getFullYear();
+  const year = date.getFullYear().toString().slice(2);
   const month = date.getMonth() + 1;
   const day = date.getDate();
   return `${month}${day}${year}`;
 }
 
-async function getReadings(dateInFormat: string) {
+async function getOrdinaryReadings(dateInFormat: string) {
   const response = await fetch(
     `https://bible.usccb.org/bible/readings/${dateInFormat}.cfm`
   );
-  //Finish scraping
-  return response;
+  if (!response.ok) {
+    throw new Error(`Network response was not ok: ${response.statusText}`);
+  }
+  const html = await response.text();
+  const firstReading = html
+    .split(`<h3 class="name">Reading 1 </h3>`)[1]
+    .split('<a href="')[1]
+    .split('">')[1]
+    .split("</a>")[0];
+
+  const responsorialPsalm = html
+    .split(`<h3 class="name">Responsorial Psalm </h3>`)[1]
+    .split('<a href="')[1]
+    .split('">')[1]
+    .split("</a>")[0];
+
+  const secondReading = html
+    .split(`<h3 class="name">Gospel </h3>`)[1]
+    .split('<a href="')[1]
+    .split('">')[1]
+    .split("</a>")[0];
+  return {
+    reading: firstReading,
+    psalm: responsorialPsalm,
+    gospel: secondReading,
+  };
 }
 
 const today = getDateInScraperFormat();
-getReadings(today).then((res) => console.log(res));
+getOrdinaryReadings(today).then((res) => console.log(res));
