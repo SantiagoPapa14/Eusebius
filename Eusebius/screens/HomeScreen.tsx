6f7fe1a69fa6dc14363d19a8b3a2a80e1c029d68
Santiago Papa <papa.santiago321@gmail.com>
2024-10-19 18:42:32 -0300

    New main menu, fixed transparency bug

--- Eusebius/screens/HomeScreen.tsx
+++ Eusebius/screens/HomeScreen.tsx
@@ -1,9 +1,64 @@
 import React from "react";
-import { Button } from "react-native";
+import {
+  View,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/Feather";
+
+const MenuButton: React.FC<{ page: string; iconName: string }> = ({
+  page,
+  iconName,
+}) => {
+  const navigation = useNavigation(); // Make sure to import and use useNavigation from react-navigation
+
+  return (
+    <TouchableOpacity
+      className="flex-row h-20 justify-center items-center bg-white rounded-full mx-10 pl-10"
+      onPress={() => navigation.navigate(page)}
+    >
+      <Icon name={iconName} size={60} color={"black"} />
+      <Text className="flex-1 ml-10 text-4xl">{page}</Text>
+    </TouchableOpacity>
+  );
+};
 
 const HomeScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
   return (
-    <Button title="Readings" onPress={() => navigation.navigate("Readings")} />
+    <View className="flex-1">
+      <ImageBackground
+        source={require("../assets/MichaelWpp.jpg")}
+        className="flex-1"
+        resizeMode="cover"
+        style={{ opacity: 0.1 }}
+      />
+      <View
+        className="flex-1 flex justify-center space-y-10 pt-10 pb-20"
+        style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
+      >
+        <View className="flex-1 justify-center items-center bg-white rounded-full mx-5 px-10 border">
+          <Image
+            source={require("../assets/eusebiusLogo.png")}
+            className="w-full h-full object-cover rounded-full"
+          />
+        </View>
+        <View className="flex-1">
+          <MenuButton page={"Readings"} iconName={"book-open"} />
+        </View>
+        <View className="flex-1">
+          <MenuButton page={"Professor"} iconName={"user"} />
+        </View>
+        <View className="flex-1">
+          <MenuButton page={"Review"} iconName={"trending-up"} />
+        </View>
+        <View className="flex-1">
+          <MenuButton page={"Knowledge"} iconName={"archive"} />
+        </View>
+      </View>
+    </View>
   );
 };
 
--- Eusebius/screens/ReadingScreen.tsx
+++ Eusebius/screens/ReadingScreen.tsx
@@ -104,50 +104,70 @@ const ReadingScreen = () => {
 
   if (loading) {
     return (
-      <ImageBackground
-        source={require("../assets/MichaelWpp.jpg")}
-        className="flex-1"
-        resizeMode="cover"
-      >
-        <View className="flex-1 bg-gray-100 opacity-95">
+      <View className="flex-1">
+        <ImageBackground
+          source={require("../assets/MichaelWpp.jpg")}
+          className="flex-1"
+          resizeMode="cover"
+          style={{ opacity: 0.1 }}
+        />
+        <View
+          className="flex-1 flex justify-center"
+          style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
+        >
           <View className="flex-1 h-screen items-center justify-center bg-gbGray">
             <View className="flex-1 items-center justify-center w-screen mt-10 pr-5 pl-5">
-              <SkeletonLoader></SkeletonLoader>
+              <Text className="text-lg text-center">
+                <SkeletonLoader />
+              </Text>
             </View>
 
+            {/* Verse Navigation */}
             <View className="flex-row justify-center items-center rounded-full bg-white h-20 w-3/4">
-              <TouchableOpacity className="mx-2">
-                <Icon name="arrow-back" size={30} color={"white"} />
+              <TouchableOpacity className="mx-2" disabled>
+                <Icon name="arrow-back" size={30} color="gray" />
               </TouchableOpacity>
               <View className="flex-1">
                 <Text className="flex text-lg items-center text-center">
                   Loading...
                 </Text>
               </View>
-
-              <TouchableOpacity className="mx-2">
-                <Icon name="arrow-forward" size={30} color={"white"} />
+              <TouchableOpacity className="mx-2" disabled>
+                <Icon name="arrow-forward" size={30} color="gray" />
               </TouchableOpacity>
             </View>
 
+            {/* Placeholder for English Content */}
             <View className="flex-1 items-center justify-center w-screen mb-10 pr-5 pl-5">
-              <SkeletonLoader></SkeletonLoader>
+              <Text className="text-lg text-center">
+                <SkeletonLoader />
+              </Text>
             </View>
 
+            {/* Reading Selector */}
             <View className="flex-row justify-center items-center bg-white h-20 w-screen">
-              <TouchableOpacity className="flex-1 items-center justify-center h-20">
+              <TouchableOpacity
+                className="flex-1 items-center justify-center h-20"
+                disabled
+              >
                 <Text>Psalm</Text>
               </TouchableOpacity>
-              <TouchableOpacity className="flex-1 items-center justify-center h-20">
+              <TouchableOpacity
+                className="flex-1 items-center justify-center h-20"
+                disabled
+              >
                 <Text className="font-bold text-base">Gospel</Text>
               </TouchableOpacity>
-              <TouchableOpacity className="flex-1 items-center justify-center h-20">
+              <TouchableOpacity
+                className="flex-1 items-center justify-center h-20"
+                disabled
+              >
                 <Text>Reading</Text>
               </TouchableOpacity>
             </View>
           </View>
         </View>
-      </ImageBackground>
+      </View>
     );
   }
 
@@ -165,12 +185,17 @@ const ReadingScreen = () => {
     readings[selectedReading].verses.end;
 
   return (
-    <ImageBackground
-      source={require("../assets/MichaelWpp.jpg")}
-      className="flex-1"
-      resizeMode="cover"
-    >
-      <View className="flex-1 bg-gray-100 opacity-95">
+    <View className="flex-1">
+      <ImageBackground
+        source={require("../assets/MichaelWpp.jpg")}
+        className="flex-1"
+        resizeMode="cover"
+        style={{ opacity: 0.1 }}
+      />
+      <View
+        className="flex-1 flex justify-center"
+        style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
+      >
         <View className="flex-1 h-screen items-center justify-center bg-gbGray">
           {/* Animated Latin Content (Sliding and Fading) */}
           <Animated.View
@@ -294,7 +319,7 @@ const ReadingScreen = () => {
           </View>
         </View>
       </View>
-    </ImageBackground>
+    </View>
   );
 };
 
